a4161421154c8463b7ede86ce4fd3ae4eaea7997
Alvaro De La Torre <alvarodltp@gmail.com>
2018-09-24 18:52:48 -0400

    workout history render

--- src/components/WorkoutContainer.js
+++ src/components/WorkoutContainer.js
@@ -5,7 +5,7 @@ import WorkoutHistory from './WorkoutHistory'
 class WorkoutContainer extends React.Component {
   render(){
     return(
-    <div>
+    <div id="workout-container">
       <WorkoutHistory />
       <WorkoutDetail newWorkout={this.props.newWorkout} addExerciseToWorkout={this.props.addExerciseToWorkout}/>
     </div>
--- src/components/WorkoutHistory.js
+++ src/components/WorkoutHistory.js
@@ -1,11 +1,18 @@
 import React from 'react'
+import { Dropdown } from 'semantic-ui-react'
+
 
 class WorkoutHistory extends React.Component {
+
+  workouts = () => {
+    this.props.workouts.map(workout => {workout.name})
+  }
+
   render(){
-    console.log(this.props.workouts)
     return(
-      <div>
-        <h1>Workout History</h1>
+      <div id="workout-history">
+        <h1>Your Past Workouts</h1>
+        <Dropdown placeholder='Skills' fluid multiple selection options={this.workouts} />
         {this.props.workouts.map(workout => <li>{workout.date}</li>)}
       </div>
     )
